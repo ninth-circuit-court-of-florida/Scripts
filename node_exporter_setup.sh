0e1df1450b5ebe2c8862c211feb1ea6d56db5896
Justin Edwards <justinledwards@gmail.com>
2022-04-06 10:00:44 -0400

    Update node_exporter_setup.sh

--- node_exporter_setup.sh
+++ node_exporter_setup.sh
@@ -9,9 +9,10 @@ apt-get install wget
 useradd node_exporter -s /sbin/nologin
 cd /tmp/
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
-tar xvfz node_exporter-*.*-amd64.tar.gz
-cp node_exporter-*.*-amd64/node_exporter /usr/sbin/
+tar xvfz node_exporter-*.*.tar.gz
+cp node_exporter-*.*/node_exporter /usr/sbin/
 
+rm -rf /etc/systemd/system/node_exporter.service
 touch /etc/systemd/system/node_exporter.service
 tee -a /etc/systemd/system/node_exporter.service > /dev/null <<EOT
 [Unit]
