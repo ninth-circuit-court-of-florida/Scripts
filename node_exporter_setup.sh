703bf5b5ed27dd293bb911868ca33bf4e9fd21b2
Justin Edwards <justinledwards@gmail.com>
2022-04-06 09:54:27 -0400

    typo

--- node_exporter_setup.sh
+++ node_exporter_setup.sh
@@ -8,7 +8,7 @@ apt-get install wget
 
 useradd node_exporter -s /sbin/nologin
 cd /tmp/
-wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-atmd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 tar xvfz node_exporter-*.*-amd64.tar.gz
 cp node_exporter-*.*-amd64/node_exporter /usr/sbin/
 
