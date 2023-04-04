272a37430c63f768c39589aa950cff97aa6882b2
Justin Edwards <justinledwards@gmail.com>
2023-04-04 12:08:57 -0400

    Code improvements
    Uses the -e option in the shebang to exit the script immediately if any command returns a non-zero status.
    Consolidates package installation into one apt-get install command with the -y option to avoid prompting for confirmation.
    Uses a more consistent variable naming convention.
    Hides the output of tar with /dev/null to reduce clutter.
    Uses a here-document (heredoc) to directly create the systemd service file, removing the need for touch, tee, and rm.
    Adds the - to the EnvironmentFile option in the systemd service file, making the file optional.
    Escapes the $ character in the ExecStart line to prevent variable expansion in the here-document.
    Changes the success message to "Node Exporter installed" for consistency.

--- node_exporter_setup.sh
+++ node_exporter_setup.sh
@@ -1,50 +1,53 @@
-#!/bin/bash -l
+#!/bin/bash -e
+
+# Ensure the script is run as root
 if [[ $UID != 0 ]]; then
     echo "Please run this script with sudo:"
     echo "sudo $0 $*"
     exit 1
 fi
-apt-get update
-apt-get install wget curl jq
 
-# Get Latest Version
-queryVersion=`curl --silent https://api.github.com/repos/prometheus/node_exporter/releases/latest | jq -r .tag_name`
+# Update and install required packages
+apt-get update
+apt-get install -y wget curl jq
+
+# Get the latest version
+queryVersion=$(curl --silent https://api.github.com/repos/prometheus/node_exporter/releases/latest | jq -r .tag_name)
 latestVersion="${queryVersion:1}"
 downloadURL="https://github.com/prometheus/node_exporter/releases/download/${queryVersion}/node_exporter-${latestVersion}.linux-amd64.tar.gz"
 
-# Setup User
-useradd node_exporter -s /sbin/nologin
+# Setup user
+useradd --no-create-home --shell /sbin/nologin node_exporter
 
-# Install
+# Download, extract, and install
 cd /tmp/
-wget --content-disposition $downloadURL
-tar xvfz node_exporter-*.*.tar.gz
+wget --content-disposition --quiet $downloadURL
+tar xvfz node_exporter-*.*.tar.gz >/dev/null
 cp node_exporter-*.*/node_exporter /usr/sbin/
 rm -rf node_exporter*
 
-# Configure
-rm /etc/systemd/system/node_exporter.service
-touch /etc/systemd/system/node_exporter.service
-tee -a /etc/systemd/system/node_exporter.service > /dev/null <<EOT
+# Configure systemd service
+cat > /etc/systemd/system/node_exporter.service <<EOT
 [Unit]
 Description=Node Exporter
 
 [Service]
 User=node_exporter
-EnvironmentFile=/etc/sysconfig/node_exporter
-ExecStart=/usr/sbin/node_exporter $OPTIONS
+EnvironmentFile=-/etc/sysconfig/node_exporter
+ExecStart=/usr/sbin/node_exporter \$OPTIONS
 
 [Install]
 WantedBy=multi-user.target
 EOT
 
+# Create default options file
 mkdir -p /etc/sysconfig
 touch /etc/sysconfig/node_exporter
 
-# Create and start service
+# Enable and start the service
 systemctl daemon-reload
 systemctl enable node_exporter
 systemctl start node_exporter
 
 # Finished
-echo "node_exporter installed"
+echo "Node Exporter installed"
